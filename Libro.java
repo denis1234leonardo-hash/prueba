7cf217b6163f239825cc1cc851332a88c6a6c354
leonardo <denis1234leonardo@gmail.com>
2026-09-15 17:57:47 -0500

    agrega metodo getInfoen Libro

--- Libro.java
+++ Libro.java
@@ -43,4 +43,9 @@ public class Libro {
     public String toString() {
         return "Libro: " + titulo + " | Autor: " + autor + " | Prestado: " + prestado;
     }
+    
+    public String getInfo() {
+    return titulo + " - " + autor;
+    }
+    
 }
