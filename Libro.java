c41a884ba9ac838f2bde6b08641f80220c596994
leonardo <denis1234leonardo@gmail.com>
2026-09-15 18:17:34 -0500

    Revert " cambia el formato de to string en Libro"
    This reverts commit 1a6a1b525a9115bf620c3a7c2dfd5d6b3ca85779.

--- Libro.java
+++ Libro.java
@@ -41,7 +41,7 @@ public class Libro {
 
     @Override
     public String toString() {
-    return "LIBRO: " + titulo + " | AUTOR: " + autor + " | PRESTADO: " + prestado;
+        return "Libro: " + titulo + " | Autor: " + autor + " | Prestado: " + prestado;
     }
 
     
